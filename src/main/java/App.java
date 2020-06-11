118954fd289d285bd0ed1451a37c9fe2a4ef5cf6
bjastrzebs002 <bartek.jastrzebski0010@gmail.com>
2020-06-11 11:35:39 +0200

    Fix main class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,15 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
 import lib.ProductsLoader;
 
 
-public class App extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+public class App {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    public App() throws Exception {
         ProductsLoader pl = new ProductsLoader();
         pl.loadProducts();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new App();
     }
 }
